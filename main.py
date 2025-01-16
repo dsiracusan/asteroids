bd7faf54ce5e5614a0ef93eb2ef8e5871834c2af
dsiracusan <dsiracusan@gmail.com>
2025-01-16 15:16:16 -0800

    add base code for asteroids

---
+++ main.py
@@ -0,0 +1,29 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+black=(0,0,0)
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill(black)
+
+    pygame.display.flip()
+
+    while True:
+        print('running')
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+    
+    if __name__ == "__main__":
+        print("Starting asteroids!")
+        print(f"Screen width: {SCREEN_WIDTH}")
+        print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+
+main()
